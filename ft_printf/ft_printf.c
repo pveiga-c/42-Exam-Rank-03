5af57423d8e105f8cc9c6747b8d994c441a4a5c1
pveiga-c <correiapc@hotmail.com>
2023-09-15 09:16:14 +0100

    ft_printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,49 +1,56 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-void ft_str_null(char *str, int *leng)
+void    ft_str(char *str, int *len)
 {
-    if (str == NULL)
-        str = "(null)";
-    while (*str)
-        *leng += write(1, str++, 1);
-}
-
-void hex_digit(long long int nbr, int base, int *leng)
-{
-    if (nbr < 0)
+    if(*str == NULL)
+        *str = "(null)";
+    while(*str)
     {
-        nbr *= -1;
-        *leng += write(1, "-", 1);
+        write(1, *str, 1);
+        *len++;
     }
-    if (nbr >= base)
-        hex_digit((nbr / base), base, leng);
-    *leng += write(1, &"0123456789abcdef"[nbr % base], 1);
 }
 
-int ft_printf(const char *str, ...)
+void    hex_digit(long long int num, int base, int *len)
 {
-    int leng = 0;
-
-    va_list pointer;
-    va_start(pointer, str);
-
-    while (*str)
+    if(num < 0)
     {
-        if ((*str == '%') && *(str + 1))
+        num = num * -1;
+        write(1, "-", 1);
+        *len++;
+    }
+    if(num >= base)
+        hex_digit((num / base), base, &len);
+    write(1, &"0123456789abcdef"[num % base], 1);
+    *len++;
+}
+
+int ft_print(const char *str, ...)
+{
+    int len;
+
+    len = 0;
+    va_list(pointer);
+    va_start(pointer, str);
+    while(*str)
+    {
+        if(*str == '%' && (*str + 1))
         {
-            str++;
-            if (*str == 's')
-                ft_str_null(va_arg(pointer, char *), &leng);
-            else if (*str == 'd')
-                hex_digit((long long int)va_arg(pointer, int), 10, &leng);
-            else if (*str == 'x')
-                hex_digit((long long int)va_arg(pointer, unsigned int), 16, &leng);
+            *str++;
+            if(str == 's')
+                ft_str(va_arg(pointer, char *), &len);
+            if(str == 'd')
+                hex_digit((long long int)(va_arg(pointer, int)), 10, &len);
+            if(str == 'x')
+                hex_digit((long long int)(va_arg(pointer, unsigned int)), 16, &len);
         }
         else
-            leng += write(1, str, 1);
-        str++;
+        {
+            write(1, str, 1);
+            len++;
+        } 
     }
     va_end(pointer);
-    return (leng);
+    return (len);
 }
