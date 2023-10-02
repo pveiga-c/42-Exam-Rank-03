05013370e8a3bf9430867e3b62ef327c29468b40
Pedro Correia <correiapc@hotmail.com>
2023-10-02 15:52:08 +0100

    study

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,84 +1,102 @@
 #include "get_next_line.h"
 
-size_t  ft_strlen(char *str)
+size_t	ft_strlen(char *s)
 {
-    size_t i;
+	size_t	len;
 
-    i = 0;
-    if(!str)
-        return(0);
-    while(str[i])
-        i++;
-    if(str[i] == '\n')
-        return(i + 1);
-    return(i);
+	len = 0;
+	if (!s)
+		return (0);
+	while (s[len])
+		len++;
+	if (s[len] == '\n')
+		return (len + 1);
+	return (len);
 }
 
-char *strjoin_line(char *s1, char *s2)
+char	*strjoin_line(char *s1, char *s2)
 {
-    size_t i;
-    size_t j;
-    char *s3;
+	char	*s3;
+	size_t	i;
+	size_t	j;
 
-    s3 = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-    if(!s3)
-        return(NULL);
-    i = 0;
-    j = 0;
-    while(s1 && s1[i])
-        s3[j++] = s1[i++];
-    i = 0;
-    while(s2[i])
-    { 
-        s3[j++] = s2[i];
-        if(s2[i++] == '\n')
-            break;
-    }
-    s3[j] = '\0';
-    free(s1);
-    return(s3);
+	s3 = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!(s3))
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1 && s1[i])
+		s3[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+	{
+		s3[j++] = s2[i];
+		if (s2[i++] == '\n')
+			break ;
+	}
+	s3[j] = '\0';
+	free(s1);
+	return (s3);
 }
 
-
-int clear_buffer(char *buffer)
+size_t	clean_line(char *buffer)
 {
-    size_t i;
-    size_t j;
-    size_t result;
+	size_t	i;
+	size_t	j;
+	size_t	result;
 
-    i = 0;
-    j = 0;
-    result = 0;
-    while(buffer[i])
-    {
-        if(result)
-            buffer[j++] = buffer[i];
-        if(buffer[i] == '\n')
-            result = 1;
-        buffer[i] = 0;
-        i++;
-    }
-    return(result);
+	i = 0;
+	j = 0;
+	result = 0;
+	while (buffer[i])
+	{
+		if (result)
+			buffer[j++] = buffer[i];
+		if (buffer[i] == '\n')
+			result = 1;
+		buffer[i] = 0;
+		i++;
+	}
+	return (result);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
+	static char	buffer[BUFFER_SIZE + 1];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	line = NULL;
+	while (buffer[0] || read(fd, buffer, BUFFER_SIZE) > 0)
+	{
+		line = strjoin_line(line, buffer);
+		if (clean_line(buffer) == 1)
+			break ;
+		if (read(fd, buffer, BUFFER_SIZE) < 0)
+		{
+			free (line);
+			return (NULL);
+		}
+	}
+	return (line);
+}
+
+int main() {
+    int fd;
     char *line;
-    static char buffer[BUFFER_SIZE + 1];
 
-    if(fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    line = NULL;
-    while (buffer[0] || read(fd, buffer, BUFFER_SIZE) > 0)
+    fd = open("test", O_RDONLY);
+    if (fd < 0) 
     {
-        line = strjoin_line(line, buffer);
-        if(clear_buffer(buffer) == 1)
-            break;
-        if(read(fd, buffer, BUFFER_SIZE) < 0)
-        {
-            free(line);
-            return(NULL);
-        }
+        perror("Erro ao abrir o arquivo");
+        return 1;
     }
-    return(line);
+    while ((line = get_next_line(fd)) != NULL) 
+    {
+        printf("Linha lida: %s\n", line);
+        free(line);
+    }
+    close(fd);
+    return 0;
 }
